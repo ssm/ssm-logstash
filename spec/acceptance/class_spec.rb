855bf5385484a864c84db57ccc605dff204d1fbd
Stig Sandbeck Mathisen <ssm@fnord.no>
2016-01-11 16:19:38 +0100

    Set up acceptance test

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -5,7 +5,28 @@ describe 'logstash class' do
     # Using puppet_apply as a helper
     it 'should work idempotently with no errors' do
       pp = <<-EOS
-      class { 'logstash': }
+      yumrepo { 'logstash':
+        descr    => 'logstash repo for centos',
+        baseurl  => 'https://packages.elastic.co/logstash/2.1/centos',
+        gpgcheck => '1',
+        enabled  => '1',
+        gpgkey   => 'https://packages.elastic.co/GPG-KEY-elasticsearch',
+      }
+      class { '::java': distribution => 'jre' }
+      class { '::logstash': }
+      logstash::instance { 'test01': }
+
+      Class['java'] -> Class['logstash']
+      Yumrepo['logstash'] -> Class['logstash']
+
+      logstash::config { 'input':
+        instance => 'test01',
+        content  => 'input { syslog { port => 10514 } }'
+      }
+      logstash::config { 'output':
+        instance => 'test01',
+        content  => 'output { stdout { } }'
+      }
       EOS
 
       # Run it twice and test for idempotency
@@ -17,9 +38,14 @@ describe 'logstash class' do
       it { is_expected.to be_installed }
     end
 
-    describe service('logstash') do
+    describe service('logstash@test01') do
       it { is_expected.to be_enabled }
       it { is_expected.to be_running }
+    end
+
+    describe file('/etc/logstash/test01.conf') do
+      its(:content) { is_expected.to match(/input.*syslog.*10514/) }
+      its(:content) { is_expected.to match(/output.*stdout/) }
     end
   end
 end
